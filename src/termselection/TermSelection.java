50eeccc7712d1d10a250c63848616392824c2cfa
thalesfc@boitata <thalesfc@boitata>
2013-09-09 13:36:27 -0300

    Adding the term selection skeleton.

---
+++ src/termselection/TermSelection.java
@@ -0,0 +1,20 @@
+package termselection;
+
+
+public class TermSelection {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO load the terms from the description of all items
+		
+		// TODO compute the score for the given metrics (CHI2, KLD, MI, DICE)
+		
+		/* TODO compute the new representation of items and users
+		 * ( using the k highly scored terms )
+		*/
+		
+		// TODO save this terms for all items and all users
+	}
+}
